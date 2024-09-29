d750a1eb9b2f336b702e0dbc044af953ab796934
Aswin-vj <146099495+aswin-v-j@users.noreply.github.com>
2024-09-29 23:36:17 +0530

    Add files via upload

--- C++/pattern21.cpp
+++ C++/pattern21.cpp
@@ -4,24 +4,20 @@ using namespace std;
 
 int main()
 {
-    int n = 7;
-    for (int i = 1; i <= n; i++)
+    int n=5;
+    for (int i = 0; i < n; i++)
     {
-        cout << "*  " ;
-         
-        for (int j = 0; j < n - 2; j++)
+        for (int j = 0; j < n; j++)
         {
-            if (i == 1 || i == n)
+            if (i==0 || i== n-1 || j==0 || j==n-1)
             {
                 cout<<"*  ";
             }
             else cout<<"   ";
         }
-
-        cout << "*  ";
-
         cout<<endl;
     }
+    
 
     return 0;
 }
